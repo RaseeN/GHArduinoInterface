08586ae09a64f0f67cc33c0f7862a5aebfbeda16
Mohammed Raseen <raseen@linux.com>
2018-02-17 02:44:19 +0530

    Added Automode capability and fixed Pylint errors

--- greenhouseController.py
+++ greenhouseController.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import mysql.connector  # Library for database
-import serial   # Library for serial communication
 import time     # Library for the delay
 
+import mysql.connector  # Library for database
 from mysql.connector import errorcode
 
-''' Change this to function '''
+import serial   # Library for serial communication
+
 try:
-    ser = serial.Serial('/dev/cu.usbmodemFA131', 9600, timeout=0.5)
+    serial = serial.Serial('/dev/cu.usbmodemFA131', 9600, timeout=0.5)
 except:
-    print("Exception in serial port")
-    ser = None
+    print "Exception in serial port"
+    serial = None
 
 time.sleep(4)   # we wait a little, so that the Arduino is ready.
-
 
 def open_green_db():
     """ Function to open Database connection and database handle. """
@@ -24,11 +23,11 @@ def open_green_db():
                                       database='db_green_test')
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
+            print "Something is wrong with your user name or password"
         elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
+            print "Database does not exist"
         else:
-            print(err)
+            print err
         return None
     else:
         return cnx
@@ -111,13 +110,67 @@ def get_device_id():
     close_green_db(cnx)
     return device_id
 
+
+def get_int_temp_threshhold():
+    """ Function to get Internal Temperature Set Point and Delta values """
+    cnx = open_green_db()
+    if cnx is None:
+        return None
+    cursor = cnx.cursor()
+    query = "SELECT SetPoint, DeltaT from types where Type = 'Temperature_int'"
+    cursor.execute(query)
+
+    setpoint, delta = None, None
+    for SetPoint, DeltaT in cursor:
+        setpoint = SetPoint
+        delta = DeltaT
+
+    return setpoint, delta
+
+
+def get_luminosity_threshold():
+    """ Function to get Luminosity Low and High Threshold values """
+    cnx = open_green_db()
+    if cnx is None:
+        return None
+
+    cursor = cnx.cursor()
+    query = "SELECT LowThreshold, HighThreshold from types where Type = 'Luminosity'"
+    cursor.execute(query)
+
+    low, high = None, None
+    for SetPoint, DeltaT in cursor:
+        low = SetPoint
+        high = DeltaT
+
+    return low, high
+
+
+def get_moisture_threshold():
+    """ Function to get Moisture Low and High Threshold values """
+    cnx = open_green_db()
+    if cnx is None:
+        return None
+
+    cursor = cnx.cursor()
+    query = "SELECT LowThreshold, HighThreshold from types where Type = 'Moisture'"
+    cursor.execute(query)
+
+    low, high = None, None
+    for LowThreshold, HighThreshold in cursor:
+        low = LowThreshold
+        high = HighThreshold
+
+    return low, high
+
+
 def process_command_request():
     """ Process Command Request from website and update Active state accordingly """
     state = get_equipment_state()
     command_request = get_equipment_command()
 
-    if ser is None:
-        print("Error in accessing Serial port!")
+    if serial is None:
+        print "Error in accessing Serial port!"
         exit(1)
 
     if state is None:
@@ -181,8 +234,8 @@ def init_device_state():
     """ Function to  initiate Device state based on Configured from Database"""
     state = get_equipment_state()
 
-    if ser is None:
-        print("Error in accessing Serial port!")
+    if serial is None:
+        print "Error in accessing Serial port!"
         return
 
     if state is None:
@@ -215,13 +268,13 @@ def send_command_arduino(command):
     ACK = ""
     while True:
         try:
-            ser.write(command)
-            ser.flush()
-            ACK = str(ser.readline())
+            serial.write(command)
+            serial.flush()
+            ACK = str(serial.readline())
             if not ACK == "OK":
                 break
         except:
-            print("send_command_arduino: Exception in serial port")
+            print "send_command_arduino: Exception in serial port"
 
 
 def update_state_command(equipment, state):
@@ -242,9 +295,9 @@ def get_measure_arduino(command):
 
     while True:
         try:
-            ser.write(command)
-            ser.flush()
-            value = ser.readline()
+            serial.write(command)
+            serial.flush()
+            value = serial.readline()
             if not value == "":
                 break
         except:
@@ -262,7 +315,8 @@ def update_measure_db(equipment_id, value):
 
     cursor = cnx.cursor()
     cursor.execute("INSERT INTO mesures(IdType, Date, Value) VALUES(%s,%s,%s)",
-                   (equipment_id, time.strftime('%y/%m/%d %H:%M:%S', time.localtime()), float(value)))
+                   (equipment_id, time.strftime('%y/%m/%d %H:%M:%S',
+                                                time.localtime()), float(value)))
     cnx.commit()
     close_green_db(cnx)
 
@@ -291,6 +345,102 @@ def process_measurement():
         update_measure_db(device_id.get("Moisture"), moisture)
 
 
+def process_valve_auto_mode(current_state):
+    """ Function to process Vale auto mode based on Soil Moisture. """
+    current_moisture = get_measure_arduino("MOIST")
+    low_threshold, high_threshold = get_moisture_threshold()
+
+    if low_threshold is None:
+        print "Moisture low_threshold is NULL"
+        return
+
+    if high_threshold is None:
+        print "Moisture high_threshold is NULL"
+        return
+
+    if current_state == 0:
+        # In Case of Dry State, turn valve ON
+        if low_threshold < current_moisture:
+            send_command_arduino("VALVEON")
+            print "VALVE ON"
+            update_state_command("Valve", 1)
+
+    if current_state == 1:
+        # In Case of Wet State, turn Valve OFF
+        if high_threshold >= current_moisture:
+            send_command_arduino("VALVEOFF")
+            print "VALVE OFF"
+            update_state_command("Valve", 0)
+
+
+def process_fan_auto_mode(current_state):
+    """ Function to trigger Fan Auto Mode and turn on and off based on temperature."""
+    current_temp_int = get_measure_arduino("TEMPINT")
+    setpoint, delta = get_int_temp_threshhold()
+
+    if setpoint is None:
+        print "Temperature Setpoint is NULL"
+        return
+
+    if delta is None:
+        print "Temperature Detla is NULL "
+        return
+
+    if current_state == 0:
+        if setpoint > current_temp_int:
+            send_command_arduino("FANON")
+            print "Fan ON"
+            update_state_command("Fan", 1)
+
+    # If Fan state is already ON, check for temperature is already below delta
+    if current_state == 1:
+        if current_temp_int < (setpoint - delta):
+            send_command_arduino("FANOFF")
+            print "Fan OFF"
+            update_state_command("Fan", 0)
+
+
+def process_lamp_auto_mode(current_state):
+    """ Function to process Lamp auto mode based on Luminosity Level. """
+    current_luminosity = get_measure_arduino("LUMIN")
+    low_threshold, high_threshold = get_luminosity_threshold()
+
+    if low_threshold is None:
+        print "Luminosity  Low Threshold is NULL"
+        return
+
+    if high_threshold is None:
+        print "Luminosity  Low Threshold is NULL"
+        return
+
+    if current_state == 0:
+        if current_luminosity < low_threshold:
+            send_command_arduino("LAMPON")
+            print "LAMP ON"
+            update_state_command("Lamp", 1)
+
+    if current_state == 1:
+        if current_luminosity >= high_threshold:
+            send_command_arduino("LAMPOFF")
+            print "Lamp OFF"
+            update_state_command("Lamp", 0)
+
+
+def process_auto_mode():
+    """ Function to process Auto Mode and triger states accordingly. """
+    device_state = get_equipment_state()
+    device_mode = get_equipment_mode()
+
+    if device_mode.get("Fan") == 0:
+        process_fan_auto_mode(device_state.get("Fan"))
+
+    if device_mode.get("Lamp") == 0:
+        process_lamp_auto_mode(device_state.get("Lamp"))
+
+    if device_mode.get("Valve") == 0:
+        process_valve_auto_mode(device_state.get("Valve"))
+
+
 if __name__ == "__main__":
     all_state = get_equipment_state()
     all_command = get_equipment_command()
@@ -305,6 +455,7 @@ if __name__ == "__main__":
         if 1 in all_mode.values():
             process_command_request()
 
+        process_auto_mode()
         process_measurement()
 
         time.sleep(1)
